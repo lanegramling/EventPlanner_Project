970427d8191df368e551c6d550df783cfa21a1dd
Damian Vu <contact@damianvu.com>
2017-10-02 11:20:12 -0500

    Helpermethods are complete. Not tested, but basically direct conversion from java to C++ w/ QT Libraries

--- EventPlanner/helpermethods.cpp
+++ EventPlanner/helpermethods.cpp
@@ -4,3 +4,85 @@ helpermethods::helpermethods()
 {
 
 }
+
+QString helpermethods::toTime(int timeslot, bool format) {
+    int hour;
+    QString min;
+
+    if (format) {
+        hour = timeslot / 2;
+    } else {
+        hour = (timeslot % 24) / 2;
+        if (hour == 0) hour = 12;
+    }
+
+    min = ((timeslot % 2) == 0) ? "00" : "30";
+
+    QString time = QVariant(hour).toString() + ":" + min;
+
+    if (format && hour < 10) time = "0" + time;
+
+    if (!format) time = (timeslot < 24) ? time + "am" : time + "pm";
+
+    return time;
+
+}
+
+QString helpermethods::toTimeSlot(int timeslot, bool format) {
+    QString result = toTime(timeslot, format) + "-" + toTime(timeslot + 1, format);
+
+    return result;
+}
+
+QString helpermethods::getTimeString(QList<int> timeslots, bool format) {
+    qSort(timeslots.begin(), timeslots.end()); // Sort list of timeslots
+
+    QString timestring = "";
+
+    int prevTime = -1;
+    int workingTimeslot = -1;
+    int slot;
+
+    foreach(slot, timeslots) {
+        if (workingTimeslot == -1) {
+            workingTimeslot = slot;
+        } else if (slot != prevTime + 1) {
+            timestring = timestring + toTimeSlot(workingTimeslot, format) + ", ";
+            workingTimeslot = slot;
+        }
+        prevTime = slot;
+    }
+
+    if (workingTimeslot != -1) {
+        timestring = timestring + toTime(workingTimeslot, format) + "-" + toTime((prevTime + 1) % 48, format);
+    }
+
+    if (workingTimeslot == 0 && prevTime == 47) timestring = "ALL DAY LONG";
+
+    if (timestring.isEmpty()) timestring = "NOT AVAILABLE AT ALL";
+
+    return timestring;
+}
+
+QString helpermethods::stringifyTimeslotInts(QList<int> timeslots) {
+    QString stringList = "";
+    int slot;
+    foreach(slot, timeslots) {
+        stringList += (timeslots.indexOf(slot) != (timeslots.size() - 1)) ? (slot + ",") : slot;
+    }
+    return stringList;
+}
+
+QList<int> helpermethods::listifyTimeslotInts(QString timeslotString) {
+    QStringList temp = timeslotString.split(',');
+
+    QString s;
+
+    QList<int> final = QList<int>();
+
+    foreach(s, temp) {
+        final.append(s.toInt());
+    }
+
+    return final;
+}
--- EventPlanner/helpermethods.h
+++ EventPlanner/helpermethods.h
@@ -1,10 +1,58 @@
 #ifndef HELPERMETHODS_H
 #define HELPERMETHODS_H
 
+#include <QList>
+#include <QString>
+#include <QVariant>
 
 class helpermethods
 {
 public:
+
+    /*!
+     * \brief toTime Will give specific time for given timeslot int
+     *        e.g. 12|24h: 27 = 1:30pm|13:30
+     * \param timeslot 0-47
+     * \param format TRUE: 24h format | FALSE: 12h format
+     * \return QString Time in 12h/24h format
+     */
+    static QString toTime(int timeslot, bool format);
+
+    /*!
+     * \brief toTimeSlot Will utilize toTime to create 30min timeslot string
+     *        e.g. Timeslot-Timeslot+1: 27 = 1:30pm-2:00pm|13:30-14:00
+     * \param timeslot 0-47
+     * \param format TRUE: 24h format | FALSE: 12h format
+     * \return
+     */
+    static QString toTimeSlot(int timeslot, bool format);
+
+    /*!
+     * \brief getTimeString Returns a smarter timeslot string based on contiguous timeslots
+     * \param timeslots Will sort then utilize list of ints with range 0-47
+     * \param format TRUE: 24h format | FALSE: 12h format
+     * \return
+     */
+    static QString getTimeString(QList<int> timeslots, bool format);
+
+    /*!
+     * \brief stringifyTimeslotInts Inverse function of listifyTimeslotInts
+     * \param timeslots QList<int>
+     * \return QString of timeslots e.g. "0,1,2,3,4"
+     */
+    static QString stringifyTimeslotInts(QList<int> timeslots);
+
+    /*!
+     * \brief listifyTimeslotInts Inverse function of stringifyTimeslotInts
+     * \param timeslotString e.g. "0,1,2,3,4"
+     * \return QList of timeslots
+     */
+    static QList<int> listifyTimeslotInts(QString timeslotString);
+
+private:
+    /*!
+     * \brief helpermethods Private constructor so helpermethods is never initialized.
+     */
     helpermethods();
 };
 
