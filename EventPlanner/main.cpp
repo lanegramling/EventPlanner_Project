81efee7898359365b7b65ceaf6e7716c50e1e59d
Lane <lanegramling@gmail.com>
2017-10-07 13:56:08 -0500

    Reworked UI for Event Admin Mode. Functionality should be complete. Added comments into the startup phase to make it easier to understand.

--- EventPlanner/main.cpp
+++ EventPlanner/main.cpp
@@ -16,14 +16,22 @@ int main(int argc, char *argv[])
      * \details Qt framework is used under <a href="https://www.gnu.org/licenses/lgpl.txt" target="_blank">LGPL</a>.
      * \details Details about the Qt classes used can be found on the <a href="http://doc.qt.io/qt-5/classes.html" target="_blank">Qt Documentation Website</a>.
      */
+
+    //Initialize Session and read save data from file
     Session session;
     session.readEventsFromFile();
+
+    //Initialize Login page
     QApplication prog(argc, argv);
     QDesktopWidget* qDesk = QApplication::desktop();
     LoginPage login(&session);
+
+    //Initialize Landing Page (w) and the 2 modes
     EventPlanner w;
     EventAdminMode mode1(&session);
     AddingMode mode2(&session);
+
+    //Set window parameters for different forms, and show Login screen
     login.setStyleSheet("QMainWindow {background: rgb(245,245,245);}");
     w.setStyleSheet("QMainWindow {background: rgb(253,253,253);}");
     mode1.setStyleSheet("QMainWindow {background: rgb(245,245,245);}");
@@ -33,14 +41,15 @@ int main(int argc, char *argv[])
     w.move((qDesk->width() - w.width())/2,(qDesk->height() - w.height())/2); // set window in the middle
     mode1.move((qDesk->width() - mode1.width())/2,(qDesk->height() - mode1.height())/2);// same
     mode2.move((qDesk->width() - mode2.width())/2,(qDesk->height() - mode2.height())/2);// same
-   login.show();
+   login.show(); //SHOW LOGIN SCREEN
 
+    //Map out the relationships between forms using signals that reference their constructors and their show methods.
     QObject::connect(&login, SIGNAL(goToEventPlanner()), &w, SLOT(receiveShow()));
     QObject::connect(&mode1,SIGNAL(showEventPlanner()),&w,SLOT(getMode1()));
     QObject::connect(&w,SIGNAL(AdminModeshow()),&mode1,SLOT(receiveshow()));
     QObject::connect(&w, SIGNAL(Logout()), &login, SLOT(receiveShow()));
 
     QObject::connect(&mode2,SIGNAL(showEventPlanner()),&w,SLOT(getMode2()));//connect signal
-    QObject::connect(&w,SIGNAL(AddingModeshow()),&mode2,SLOT(receiveshow()));//   with differen windows
+    QObject::connect(&w,SIGNAL(AddingModeshow()),&mode2,SLOT(receiveshow()));//   with different windows
     return (prog.exec());
 }
